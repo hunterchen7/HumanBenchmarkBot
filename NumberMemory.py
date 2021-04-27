f6640310c903aa9050df770c793676f09de5ac51
Hunter Chen <chenhunter0707@gmail.com>
2021-04-27 01:50:25 -0600

    some dummy JS

--- NumberMemory.py
+++ NumberMemory.py
@@ -1,1 +1,15 @@
 # And with some testing, it seems that it's here where we've reached the limits of OpenCV OCR...
+
+# here's some JS I wrote that should help me get started..
+
+'''
+var jqry = document.createElement('script');
+jqry.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(jqry);
+jQuery.noConflict();
+
+function get_num() {
+    console.log($('.big-number').text());
+}
+var test = setInterval(get_num, 1000);
+'''
